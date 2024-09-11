527bea5244b42009ed7135f9ef922bd3ac18db05
Mikhail Saburov <saburov1985@gmail.com>
2024-09-11 23:02:12 +1100

    объеденил роуты согласно REST

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # маршрут для отправки зароса
-  post '/questions', to: 'questions#create'
-  patch '/questions/:id', to: 'questions#update'
-  delete '/questions/:id', to: 'questions#destroy'
+  resource :questions
 end
