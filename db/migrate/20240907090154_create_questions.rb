98d9e65cd259044620fff71932c4d0eadf889540
Mikhail Saburov <saburov1985@gmail.com>
2024-09-11 20:58:12 +1100

    миграция модели questions

--- db/migrate/20240907090154_create_questions.rb
+++ db/migrate/20240907090154_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[7.2]
   def change
     create_table :questions do |t|
       t.text :body
-      t.integer :user_id
+      t.integer :user_id 
 
       t.timestamps
     end
