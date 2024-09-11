b923d53ee02e0012786a7ff8acf5b4b7b4513980
Mikhail Saburov <saburov1985@gmail.com>
2024-09-11 22:57:18 +1100

    реализовать методы обновления и удаления вопросов

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # маршрут для отправки зароса
+  post '/questions', to: 'questions#create'
+  patch '/questions/:id', to: 'questions#update'
+  delete '/questions/:id', to: 'questions#destroy'
 end
